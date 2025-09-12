d70bac3a1448bfe9a02e9656ea2165642c336fc3
KZ <hikicrypto@gmail.com>
2025-09-12 06:47:37 +0300

    Update Modifiers.ts

--- Types/Modifiers.ts
+++ Types/Modifiers.ts
@@ -16,3 +16,21 @@ type Partial<T> = {
 type Required<T> = {
   [P in keyof T]-?: T[P];
 };
+
+
+class Person {
+  id: number;
+  name: string;
+  age: number;
+  constructor(id: number, name: string, age: number) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+  }
+}
+
+type RemoveProps<T, K> = Exclude<T, K>;
+// type Person but without name and age
+type RemainProps<T, K> = Exclude<keyof T, K>;
+type PersonBlindAuditions = Omit<Person01, 'name' | 'age'>;
+const p: PersonBlindAuditions = { id: 1 };
