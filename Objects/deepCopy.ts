fda96b56e1d146f995b35fdb37a478e6600f001d
kaize <hikicrypto@gmail.com>
2025-09-21 07:49:11 +0300

    Update deepCopy.ts

--- Objects/deepCopy.ts
+++ Objects/deepCopy.ts
@@ -5,3 +5,14 @@ deepCopy.name = 'Alice';
 deepCopy.details.city = 'Los Angeles';
 
 console.log(original.details.city); // "New York"
+
+// best copy: simple, deep copy, copies methods, copies Date, Set, Map
+import _ from "lodash";
+
+const original = { name: "John", age: 30, details: { city: "New York" } };
+const deepCopy = _.cloneDeep(original);
+
+deepCopy.name = "Alice";
+deepCopy.details.city = "Los Angeles";
+
+console.log(original.details.city); // "New York"
